7b308aecd87e7fc26b9f544f9a2038495cc43f3e
raghad <raghadalyan01@gmail.com>
2025-07-07 01:30:51 +0300

    before island class

---
+++ Kitchenbuilder.Core/Models/Island.cs
@@ -0,0 +1,9 @@
+﻿public class Island
+{
+    public int Depth { get; set; } = 90;
+    public int Width { get; set; } = 180;
+    public string Material { get; set; } = "";
+    public int Direction { get; set; } = 90;
+    public double DistanceX { get; set; }
+    public double DistanceY { get; set; }
+}
